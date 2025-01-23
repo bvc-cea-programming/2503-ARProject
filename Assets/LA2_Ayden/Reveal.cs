a39263954d6ddad3ae77e1ccb937e3a72a498457
Ayden <Theownage79@gmail.com>
2025-01-22 18:23:01 -0700

    each button press now reveals another capsule instead of all at once

--- Assets/LA2_Ayden/Reveal.cs
+++ Assets/LA2_Ayden/Reveal.cs
@@ -5,36 +5,27 @@ public class Reveal : MonoBehaviour
 {
 
     public Button button;
+    public int i = 0;
 
-    public GameObject[] objects;
+    public Cap[] objects;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        objects = FindObjectsByType<GameObject>(FindObjectsSortMode.None);
+        objects = FindObjectsByType<Cap>(FindObjectsSortMode.None);
         foreach (var obj in objects)
         {
-            if (obj.GetComponent<MeshRenderer>())
-            {
-                obj.SetActive(false);
-            }
+            obj.gameObject.SetActive(false);
         }
     }
 
     public void Press()
     {
-        foreach (var obj in objects)
+        if (i < objects.Length)
         {
-            if (obj.GetComponent<MeshRenderer>())
-            {
-                if (!obj.activeSelf)
-                {
-                    obj.SetActive(true);
-                }
-                else
-                {
-                    obj.SetActive(false);
-                }
-            }
+            var obj = objects[i];
+            obj.gameObject.SetActive(true);
+
+            i++;
         }
     }
 
