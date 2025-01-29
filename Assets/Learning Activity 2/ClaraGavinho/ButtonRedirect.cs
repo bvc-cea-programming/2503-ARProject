95ec08218e448b883614dbfc63ab22cb5ca595b8
Clara Gavinho <clara.gavinho@gmail.com>
2025-01-28 19:58:26 -0700

    LA2 Test

---
+++ Assets/Learning Activity 2/ClaraGavinho/ButtonRedirect.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class ButtonRedirect : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://i.pinimg.com/236x/03/ab/56/03ab565545ee9f4e6087429e55c7ea03.jpg");
+    }
+}
